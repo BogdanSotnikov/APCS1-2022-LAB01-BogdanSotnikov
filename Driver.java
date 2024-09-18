1029b9bdb20f3c99fc93220084f2af4eccee38a9
BogdanSotnikov <bogdans2789@nycstudents.net>
2024-09-17 22:38:07 -0400

    Sorry, Mr. Konstantinovich, when I tried using the link on your website to accept the assignment on github classroom, it said that I had no access. I have forked the repository into my account and completed the lab. You will have this commit and my Piazza post as proof that I did this before the due date.

--- Driver.java
+++ Driver.java
@@ -16,7 +16,16 @@ public class Driver{
    *The tests should print the intended output in addition to the actual output.
    *You should also replace the placeholder text with an actual function call.
    */
-   System.out.println("makeBricks(1,1,2) expected false result: " + "REPLACE THIS");
-   System.out.println("makeBricks(1,1,6) expected true  result: " + "REPLACE THIS" );
+   System.out.println("makeBricks(1,1,2) expected: false result: " + Functions.makeBricks(1, 1, 2));
+   System.out.println("makeBricks(1,1,6) expected: true  result: " + Functions.makeBricks(1, 1, 6));
+
+   System.out.println("makeBricks(10,1,16) expected: false  result: " + Functions.makeBricks(10, 1, 16));
+   System.out.println("makeBricks(11,1,16) expected: true  result: " + Functions.makeBricks(11, 1, 16));
+   System.out.println("makeBricks(1,3,16) expected: true  result: " + Functions.makeBricks(1, 3, 16));
+   System.out.println("makeBricks(0,3,27) expected: false  result: " + Functions.makeBricks(0, 3, 27));
+   System.out.println("makeBricks(27,0,27) expected: true  result: " + Functions.makeBricks(27, 0, 27));
+   System.out.println("makeBricks(0,5,25) expected: true  result: " + Functions.makeBricks(0, 5, 25));
+   System.out.println("makeBricks(25,0,25) expected: true  result: " + Functions.makeBricks(25, 0, 25));
+
  }
 }
--- Functions.java
+++ Functions.java
@@ -7,13 +7,17 @@ public class Functions{
 In this comment block, explain what your group discussed and how you wish to partition the problem. 
 List the cases, and how finding the answer differs in each case.
 Your response:
+    We thought that we needed to have enough small bricks to fill what the big bricks wouldn't be able to, which is where we got "goal%5<=small"
+    Then, we combined that with the case of having enough bricks to fulfill the goal at all, which is the other half of the boolean.
 
+    In all, our human language solution was this:
+    We have at least as many total bricks than we need and at least as many small bricks as we need.
 
  */
 
  /*STEP 2: Write the test cases first in the driver. Pay attention to how the test function is called.*/
  /*STEP 3: Complete the function makeBricks.*/
  public static boolean makeBricks(int small, int big, int goal){
-  return false;//placeholder return value; 
+  return (goal<=5*big+small) && (goal%5<=small);
  }
 }
